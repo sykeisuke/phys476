7082995a051ba93b1f46941275979d2fd931327a
Keisuke YOSHIHARA <ykeisuke@Keisukes-MacBook-Pro-11819.local>
2025-01-13 23:01:13 -1000

    change the dir structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # phys476
+Verilog codes that are used in the class are shared in this repository. 
---
+++ verilog/xor_2.v
@@ -0,0 +1,16 @@
+module XOR2(
+  input A,
+  input B,
+  output C
+);
+
+  wire [1:0] Q;
+
+  assign Q = {A, B};
+  assign C = (Q == 2'b00) ? 1'b0 :
+  	     (Q == 2'b01) ? 1'b1 :
+  	     (Q == 2'b10) ? 1'b1 :
+  	     (Q == 2'b11) ? 1'b0 : 1'b0;
+
+endmodule
+
