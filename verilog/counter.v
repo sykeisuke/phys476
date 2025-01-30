fa24937fd744ab5518800872dbce15a53a8087b7
Keisuke YOSHIHARA <ykeisuke@Keisukes-MacBook-Pro-11819.local>
2025-01-30 10:49:32 -1000

    counter

---
+++ verilog/counter.v
@@ -0,0 +1,23 @@
+module COUNTER(
+  input CLK,
+  input R,
+  input CE,
+  output reg [3:0] Q,
+  output TC,
+  output CEO
+);
+
+  always @(posedge CLK) begin
+    if (R) begin
+      Q <= 4'b0;
+    end
+    else if (CE) begin
+      Q <= Q + 1'b1;
+    end
+  end
+
+  assign TC = &Q;
+  assign CEO = &Q & CE;
+
+endmodule
+
