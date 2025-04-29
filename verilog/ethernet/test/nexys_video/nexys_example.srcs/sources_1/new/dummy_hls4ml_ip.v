44f87a2887671e0d074584506a0786470452a448
Keisuke YOSHIHARA <ykeisuke@dhcp-168-105-252-14.wireless.manoa.hawaii.edu>
2025-04-28 16:46:41 -1000

    ap proceess should take longer

--- verilog/ethernet/test/nexys_video/nexys_example.srcs/sources_1/new/dummy_hls4ml_ip.v
+++ verilog/ethernet/test/nexys_video/nexys_example.srcs/sources_1/new/dummy_hls4ml_ip.v
@@ -11,56 +11,43 @@ module dummy_hls4ml_ip (
     output reg [127:0] output_data_flat   // 4 x 32bit = 128bit
 );
 
-reg ap_start_latched; 
 reg processing;
-reg [3:0] processing_counter;
-
-always @(posedge ap_clk or negedge ap_rst_n) begin
-    if (!ap_rst_n) begin
-        ap_start_latched <= 0;
-    end else begin
-        if (ap_start)
-            ap_start_latched <= 1;
-        else if (processing)
-            ap_start_latched <= 0; 
-    end
-end
-
-// State machine
-always @(posedge ap_clk or negedge ap_rst_n) begin
-    if (!ap_rst_n) begin
-        ap_done <= 0;
-        ap_idle <= 1;
-        ap_ready <= 1;
-        processing <= 0;
-        processing_counter <= 0;
-        output_data_flat <= 0;
-    end else begin
-        if (ap_start_latched && !processing) begin
-            ap_idle <= 0;
-            ap_ready <= 0;
-            processing <= 1;
-            processing_counter <= 0;
-        end else if (processing) begin
-            if (processing_counter == 30) begin
-              // Dummy operation: just sum some inputs
-              output_data_flat[ 31:  0] <= input_data_flat[ 31:  0] + input_data_flat[ 63: 32];
-              output_data_flat[ 63: 32] <= input_data_flat[ 95: 64] + input_data_flat[127: 96];
-              output_data_flat[ 95: 64] <= input_data_flat[159:128] + input_data_flat[191:160];
-              output_data_flat[127: 96] <= input_data_flat[223:192] + input_data_flat[255:224];
-
-              ap_done <= 1;
-              ap_idle <= 1;
-              ap_ready <= 1;
-              processing <= 0;
-            end else begin
-              processing_counter <= processing_counter + 1;
-              ap_done <= 0;
-            end
-        end else begin
-            ap_done <= 0;
-        end
-    end
-end
-
-endmodule
+reg [4:0] process_counter;
+ 
+ // State machine
+ always @(posedge ap_clk or negedge ap_rst_n) begin
+     if (!ap_rst_n) begin
+         ap_done <= 0;
+         ap_idle <= 1;
+         ap_ready <= 1;
+         processing <= 0;
+         process_counter <= 0;
+         output_data_flat <= 0;
+     end else begin
+         if (ap_start && !processing) begin
+             ap_idle <= 0;
+             ap_ready <= 0;
+             processing <= 1;
+             process_counter <= 0;
+         end else if (processing) begin
+             if (process_counter == 5'd19) begin
+               // Dummy operation: just sum some inputs
+               output_data_flat[ 31:  0] <= input_data_flat[ 31:  0] + input_data_flat[ 63: 32];
+               output_data_flat[ 63: 32] <= input_data_flat[ 95: 64] + input_data_flat[127: 96];
+               output_data_flat[ 95: 64] <= input_data_flat[159:128] + input_data_flat[191:160];
+               output_data_flat[127: 96] <= input_data_flat[223:192] + input_data_flat[255:224];
+               ap_done <= 1;
+               ap_idle <= 1;
+               ap_ready <= 1;
+               processing <= 0;
+             end else begin
+               process_counter <= process_counter + 1;
+               ap_done <= 0;
+             end
+         end else begin
+             ap_done <= 0;
+         end
+     end
+ end
+ 
+ endmodule
